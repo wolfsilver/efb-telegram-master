234371ff61a5ca2c8d8e13766e99bdf2f18c9076
wolfsilver <silverwolf@foxmail.com>
2019-03-06 13:18:59 +0800

    Mod 更新群组信息，去掉emoji前缀

--- efb_telegram_master/chat_binding.py
+++ efb_telegram_master/chat_binding.py
@@ -867,7 +867,7 @@ class ChatBindingManager(LocaleMixin):
             channel_id, chat_uid = utils.chat_id_str_to_id(chats[0])
             channel = coordinator.slaves[channel_id]
             chat = ETMChat(chat=channel.get_chat(chat_uid), db=self.db)
-            bot.set_chat_title(tg_chat, chat.chat_title)
+            bot.set_chat_title(tg_chat, chat.chat_title.replace('💬👤 ', '').replace('💬👥 ', ''))
             picture = channel.get_chat_picture(chat)
             if not picture:
                 raise EFBOperationNotSupported()
